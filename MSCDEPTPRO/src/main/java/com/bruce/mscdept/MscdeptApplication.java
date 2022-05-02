a75f2fc3b8e65dadca6b9222a097b5b3331a47fc
Bruemani <rajamanikandanreg32@gmail.com>
2022-05-02 13:18:08 +0530

    hello commit-1

--- MSCDEPTPRO/src/main/java/com/bruce/mscdept/MscdeptApplication.java
+++ MSCDEPTPRO/src/main/java/com/bruce/mscdept/MscdeptApplication.java
@@ -2,9 +2,15 @@ package com.bruce.mscdept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MscdeptApplication {
+public class MscdeptApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MscdeptApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MscdeptApplication.class, args);
